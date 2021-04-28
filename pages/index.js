29a1292cc59f7489364cdedb4e46f98e48895be5
Alaa Alser <alaalsser@gmail.com>
2021-04-29 00:04:45 +0300

    fix style

--- pages/index.js
+++ pages/index.js
@@ -1,104 +1,112 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import List from '../components/List';
 import NewForm from '../components/NewForm';
 import EditForm from '../components/EditForm';
 
-
 const IndexPage = () => {
+  const [newTask, setNewTask] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editTodos, setEditTodos] = useState([]);
+  const [editTodosOriginal, setEditTodosOriginal] = useState('');
+  const [completed, setCompleted] = useState(false);
 
-const [newTask,setNewTask] = useState("")	
-const [todos,setTodos] = useState([])
-const [editMode,setEditMode] = useState(false)
-const [editTodos,setEditTodos] = useState([])
-const [editTodosOriginal,setEditTodosOriginal] = useState("")
-const [completed,setCompleted] = useState(false)
+  const onNewtodos = (event) => {
+    setNewTask(event.target.value);
+  };
 
+  const onNewFormSubmit = (event) => {
+    event.preventDefault();
+    setTodos([newTask, ...todos]);
+    setNewTask('');
+  };
 
-const onNewtodos = (event) => {
-	setNewTask(event.target.value);
-}
+  const onDelete = (deleteItem) => {
+    const deleteTask = todos.filter((item) => item !== deleteItem);
+    setTodos(deleteTask);
+  };
 
+  const onComplete = (todo) => {
+    setTodos(
+      todos.map((item) => {
+        if (item === todo) {
+          return [...todo];
+        }
+        return item;
+      })
+    );
+  };
 
-const onNewFormSubmit = (event) => {
-		event.preventDefault();
-		setTodos([newTask,...todos ]);
-		setNewTask("")
-	}
+  const onEditFormCancel = (event) => {
+    event.preventDefault();
+    setEditMode(false);
+  };
 
-const	onDelete = (deleteItem) => {
-		const deleteTask =  todos.filter( item => item !== deleteItem );
-		setTodos(deleteTask)
-	}
+  const onEditTodos = (event) => {
+    setEditTodos(event.target.value);
+  };
 
-	const onComplete = (todo) => {
-		setTodos(
-			todos.map((item)=> {
-				if(item === todo){
-					return([...todo])
-				}
-				return item
-			})
-		)
-	}
-	
-	const	onEditFormCancel = (event) => {
-		event.preventDefault();
-		setEditMode(false);
-	}
+  const onEditFormSubmit = (event) => {
+    event.preventDefault();
+    const newItems = todos.map((todo) => {
+      if (todo === editTodosOriginal) {
+        todo = editTodos;
+        setEditMode(false);
+      }
+      return todo;
+    });
+    setTodos(newItems);
+  };
 
+  const onEdit = (editItem) => {
+    setEditTodos(editItem);
+    setEditTodosOriginal(editItem);
+    setEditMode(true);
+  };
 
-const	onEditTodos = (event) => {
-	setEditTodos(event.target.value)
-	}
+  return (
+    <div>
+      <style jsx>{`
+        header {
+          background: #fbc13f;
+          margin: -10px -10px 0;
+          padding: 10px;
+        }
 
-	const	onEditFormSubmit = (event) => {
-		event.preventDefault();
-		const newItems = todos.map(todo => {
-			if(todo === editTodosOriginal) {
-				todo = editTodos;
-	 	setEditMode(false);
-}
-			return todo;
-		});
-		setTodos(newItems)
-	}
+        header h1 {
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
 
-const	onEdit = (editItem) => {
-	setEditTodos(editItem);
-	setEditTodosOriginal(editItem)
-	setEditMode(true);
+      <header>
+        <h1>To-Do List</h1>
+      </header>
 
-	}
-
-
-
-	
-	return (
-            <div>
-
-                <style jsx>{`
-					header {
-						background: #FBC13F;
-						margin: -10px -10px 0;
-						padding: 10px;
-					}
-					
-					header h1 {
-						margin: 0;
-						padding: 0;
-					}
-                `}</style>
-
-				<header>
-	                <h1>To-Do App</h1>
-				</header>
-
-				{ editMode ?( <EditForm onChange={onEditTodos} onSubmit={onEditFormSubmit} onCancel={onEditFormCancel} value={editTodos} /> ) :( 			<div>
-                <NewForm onChange={onNewtodos} onSubmit={onNewFormSubmit} value={newTask} />
-                <List items={todos} onDelete={onDelete} onEdit={onEdit} onComplete={onComplete}  />
-			</div>) }
-            </div>
-        );
-}
+      {editMode ? (
+        <EditForm
+          onChange={onEditTodos}
+          onSubmit={onEditFormSubmit}
+          onCancel={onEditFormCancel}
+          value={editTodos}
+        />
+      ) : (
+        <div>
+          <NewForm
+            onChange={onNewtodos}
+            onSubmit={onNewFormSubmit}
+            value={newTask}
+          />
+          <List
+            items={todos}
+            onDelete={onDelete}
+            onEdit={onEdit}
+            onComplete={onComplete}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default IndexPage;
