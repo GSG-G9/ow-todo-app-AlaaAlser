949abb6bbdac5816736c2a36166f685bbe58acc3
Alaa Alser <alaalsser@gmail.com>
2021-04-29 11:20:33 +0300

    fix style

--- components/EditForm.js
+++ components/EditForm.js
@@ -4,10 +4,12 @@ const EditForm = ({ value = '', onChange, onSubmit }) => {
   return (
     <form onSubmit={onSubmit}>
       <input className={styles.editinput} name="task" type="text" value={value} onChange={onChange} />
+      <div className={styles.editflex}  >
       <button className={styles.edit}  type="submit">Save</button>
         <button className={styles.edit} >
           Cancel
         </button>
+        </div>
     </form>
   );
 };
